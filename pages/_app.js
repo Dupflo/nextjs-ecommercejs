59056df0f0873ae097487f71301835148904a101
Florian Dupuis <floriandupuis1996@gmail.com>
2021-08-05 00:59:53 -0400

    Set Tailwind CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import { ThemeProvider } from "next-themes";
+import "../styles/globals.css";
 import { CartProvider } from "../context/cart";
 
 function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class">
+        <main class="dark:bg-gray-800 h-screen">
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </CartProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import { useTheme } from "next-themes";
 import commerce from "../lib/commerce";
 import CategoryList from "../components/CategoryList";
 import ProductList from "../components/ProductList";
 
 export async function getStaticProps() {
-  const merchant = await commerce.merchants.about();
+  const { data: merchant } = await commerce.merchants.about();
   const { data: categories } = await commerce.categories.list();
   const { data: products } = await commerce.products.list();
 
@@ -17,11 +18,23 @@ export async function getStaticProps() {
 }
 
 export default function IndexPage({ merchant, categories, products }) {
+  const { theme, setTheme } = useTheme();
   return (
     <div>
-      <h1>{merchant.business_name}</h1>
-      <CategoryList categories={categories} />
-      <ProductList products={products} />
+      <h1 className="text-xl  bg-green-400 text-white text-center py-2">
+        {merchant[0].name}
+      </h1>
+
+      <div className="container mx-auto my-5">
+        <CategoryList categories={categories} />
+        <ProductList products={products} />
+        <button
+          className="rounded bg-green-400 text-white py-1 px-3 my-2"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          toggle
+        </button>
+      </div>
     </div>
   );
 }
